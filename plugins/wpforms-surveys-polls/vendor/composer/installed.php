b5d4eb421b0bb5bf4d512eb7a777dfe90dfb7640
farhan90987 <farhan90987@gmail.com>
2026-01-02 21:26:24 +0500

    Some other remaing changes

---
+++ plugins/wpforms-surveys-polls/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd9ea193fe26588f5202c0f1e07c46190834250fc',
+        'name' => 'wpforms/surveys-polls',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wpforms/surveys-polls' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd9ea193fe26588f5202c0f1e07c46190834250fc',
+            'dev_requirement' => false,
+        ),
+    ),
+);
